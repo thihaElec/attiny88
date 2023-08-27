51285af5f68f371f88e2bcd2ecf78d7f7f7b369d
thihaElec <thiha.sales@gmail.com>
2023-08-27 11:37:16 +0800

    Add files via upload

---
+++ mhtiny-blink13/mhtiny-blink13/main.c
@@ -0,0 +1,22 @@
+/*
+ * mhtiny-blink13.c
+ *
+ * Created: 24/7/2023 11:17:33 pm
+ * Author : kzw
+ */ 
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRA |= (1<<DDA1); 
+
+    /* loop */
+        while (1) {
+	        PORTA ^= (1<<PORTA1);  // toggle LED pin
+	        _delay_ms(500);
+        }
+
+}
+
